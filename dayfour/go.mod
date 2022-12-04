614735b722a40265969fcce0a3d799ea33c8ef3c
Josh Rose <joshrose@hey.com>
2022-12-04 07:07:49 -0500

    bootstrap day four

---
+++ dayfour/go.mod
@@ -0,0 +1,3 @@
+module github.com/joshuarose/dayfour
+
+go 1.19
---
+++ dayfour/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	if 2+2 != 4 {
+		t.Error("placeholder for day four")
+	}
+}

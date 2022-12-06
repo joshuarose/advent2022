aeb3611dca365c1e0c68e490d47f0e3bc7a5d4a7
Josh Rose <joshrose@hey.com>
2022-12-05 19:09:48 -0500

    completed day 5 part 1

--- dayfive/main.go
+++ dayfive/main.go
@@ -1,7 +1,134 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	STACK_COUNT = 9
+	EMPTY_BOX   = "   " // an empty box is 3 spaces in a slot
+)
+
+var warehouse [9][]string
+
+func init() {
+	warehouse = [9][]string{}
+}
 
 func main() {
-	fmt.Println("start day five")
+	fmt.Println(processWarehouse())
+}
+
+// Sets up initial crate stacking from input.txt
+func processWarehouse() string {
+	// Open and process crates
+	txtfile, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln("Can't open txt file for section assignments")
+	}
+
+	txtScanner := bufio.NewScanner(txtfile)
+	txtScanner.Split(bufio.ScanLines)
+	currentRowIndex := 0
+	fileRows := make([]string, 511)
+
+	for txtScanner.Scan() {
+		fileRows[currentRowIndex] = txtScanner.Text()
+		currentRowIndex += 1
+	}
+	setupWarehouse(fileRows[0:8])
+	for _, v := range fileRows[10:511] {
+		instructions := parseInstruction(v)
+		moveCrates(instructions)
+	}
+	crateMap, letters := friendlyPrintWarehouseData(warehouse)
+	txtfile.Close()
+	fmt.Println(crateMap)
+	return letters
+}
+
+func parseRow(row string) []string {
+	crates := make([]string, STACK_COUNT)
+	spacers := []int{3, 7, 11, 15, 19, 23, 27, 31, 35}
+	for i, v := range spacers {
+		startCrate := v - 3
+		crate := row[startCrate:v]
+		crates[i] = crate
+	}
+	return crates
+}
+
+func parseInstruction(row string) []int {
+	unfilteredArray := strings.Split(row, " ")
+	moveQuantity, err := strconv.Atoi(unfilteredArray[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	startingColumn, err := strconv.Atoi(unfilteredArray[3])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	endingColumn, err := strconv.Atoi(unfilteredArray[5])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return []int{moveQuantity, startingColumn, endingColumn}
+}
+
+func moveCrates(instructions []int) {
+	cratesToMove := instructions[0]
+	fromStack := instructions[1] - 1
+	toStack := instructions[2] - 1
+	for i := 0; i < cratesToMove; i++ {
+		topCrate := warehouse[fromStack][len(warehouse[fromStack])-1]
+		warehouse[toStack] = append(warehouse[toStack], topCrate)
+		warehouse[fromStack] = warehouse[fromStack][0 : len(warehouse[fromStack])-1]
+	}
+}
+
+func setupWarehouse(rows []string) {
+	// find tallest stack
+	starterRows := [][]string{}
+	for i := range rows {
+		starterRows = append(starterRows, parseRow(rows[i]))
+	}
+	columns := [9][]string{}
+	for i := 7; i >= 0; i-- {
+		for j, v := range starterRows[i] {
+			if v != EMPTY_BOX {
+				columns[j] = append(columns[j], v)
+			}
+		}
+	}
+	warehouse = columns
+}
+
+func friendlyPrintWarehouseData(wh [9][]string) (string, string) {
+	highestStack := 0
+	topLetters := ""
+	warehouseMap := ""
+	for i := 0; i < 9; i++ {
+		if len(wh[i]) > highestStack {
+			highestStack = len(wh[i])
+		}
+		topLetters += wh[i][len(wh[i])-1]
+	}
+	for i := (highestStack); i > 0; i-- {
+		for j := 0; j < 9; j++ {
+			if len(wh[j]) >= i {
+				warehouseMap += fmt.Sprint(wh[j][i-1])
+			} else {
+				warehouseMap += fmt.Sprint(EMPTY_BOX)
+			}
+		}
+		warehouseMap += "\n"
+	}
+	topLetters = strings.ReplaceAll(topLetters, "[", "")
+	topLetters = strings.ReplaceAll(topLetters, "]", "")
+	return warehouseMap, topLetters
 }
--- dayfive/main_test.go
+++ dayfive/main_test.go
@@ -2,8 +2,101 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
-	if 1+1 != 2 {
-		t.Error("math isn't math")
+func TestParseRow(t *testing.T) {
+	cases := []struct {
+		row      string
+		expected []string
+	}{
+		{"        [H]     [W] [B]            ", []string{EMPTY_BOX, EMPTY_BOX, "[H]", EMPTY_BOX, "[W]", "[B]", EMPTY_BOX, EMPTY_BOX, EMPTY_BOX}},
+		{"    [D] [B]     [L] [G] [N]        ", []string{EMPTY_BOX, "[D]", "[B]", EMPTY_BOX, "[L]", "[G]", "[N]", EMPTY_BOX, EMPTY_BOX}},
+		{"[T] [L] [Z] [R] [C] [Q] [V] [P] [H]", []string{"[T]", "[L]", "[Z]", "[R]", "[C]", "[Q]", "[V]", "[P]", "[H]"}},
+	}
+
+	for _, tc := range cases {
+		result := parseRow(tc.row)
+		if len(result) != 9 {
+			t.Errorf("Expected 9 stacks but received: %d", len(result))
+		}
+		for i, v := range result {
+			if tc.expected[i] != v {
+				t.Errorf("Expected array element at position %d to be %s", i, v)
+			}
+		}
+	}
+}
+
+func TestParseInstruction(t *testing.T) {
+	cases := []struct {
+		row      string
+		expected []int
+	}{
+		{"move 3 from 2 to 9", []int{3, 2, 9}},
+		{"move 1 from 1 to 6", []int{1, 1, 6}},
+		{"move 13 from 7 to 6", []int{13, 7, 6}},
+	}
+	for _, tc := range cases {
+		result := parseInstruction(tc.row)
+		if len(result) != len(tc.expected) {
+			t.Error("Unexpected array length")
+		}
+		for i, v := range result {
+			if tc.expected[i] != v {
+				t.Errorf("expected %d but received %d", tc.expected[i], v)
+			}
+		}
+	}
+}
+
+func TestSetupWarehouse(t *testing.T) {
+	warehouseStrings := []string{
+		"        [H]     [W] [B]            ",
+		"    [D] [B]     [L] [G] [N]        ",
+		"[P] [J] [T]     [M] [R] [D]        ",
+		"[V] [F] [V]     [F] [Z] [B]     [C]",
+		"[Z] [V] [S]     [G] [H] [C] [Q] [R]",
+		"[W] [W] [L] [J] [B] [V] [P] [B] [Z]",
+		"[D] [S] [M] [S] [Z] [W] [J] [T] [G]",
+		"[T] [L] [Z] [R] [C] [Q] [V] [P] [H]",
+	}
+	setupWarehouse(warehouseStrings)
+	result := warehouse[0][0]
+	expected := "[T]"
+	if result != expected {
+		t.Errorf("mismatch warehouse: expected %s, received %s", expected, result)
+	}
+	result2 := len(warehouse[0])
+	expected2 := 6
+	if result2 != expected2 {
+		t.Errorf("mismatch warehouse: expected %d, received %d", expected2, result2)
+	}
+}
+
+func TestMoveCrates(t *testing.T) {
+	warehouseStrings := []string{
+		"        [H]     [W] [B]            ",
+		"    [D] [B]     [L] [G] [N]        ",
+		"[P] [J] [T]     [M] [R] [D]        ",
+		"[V] [F] [V]     [F] [Z] [B]     [C]",
+		"[Z] [V] [S]     [G] [H] [C] [Q] [R]",
+		"[W] [W] [L] [J] [B] [V] [P] [B] [Z]",
+		"[D] [S] [M] [S] [Z] [W] [J] [T] [G]",
+		"[T] [L] [Z] [R] [C] [Q] [V] [P] [H]",
+	}
+	setupWarehouse(warehouseStrings)
+	moveCrates([]int{3, 2, 9})
+	lastRow := warehouse[8]
+	expected := []string{"[H]", "[G]", "[Z]", "[R]", "[C]", "[D]", "[J]", "[F]"}
+	for i, v := range expected {
+		if lastRow[i] != v {
+			t.Errorf("Expected element %s at index %d but received %s", expected[i], i, lastRow[i])
+		}
+	}
+}
+
+func TestProcessWarehouse(t *testing.T) {
+	result := processWarehouse()
+	expected := "TLFGBZHCN"
+	if result != expected {
+		t.Errorf("expected %s received %s", expected, result)
 	}
 }
